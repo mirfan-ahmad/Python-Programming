bf624150584eae52e2ca165a5ca10c31f0399fa6
Irfan Ahmad <irfanahmad2959@gmail.com>
2022-11-06 01:43:23 +0500

    Modified code for even-odd increment problem, previous problem were exceeding time limit,,,, but this is working fine

--- Code Forces/Solutions/Level 800/1744/1744B Even-Odd Increment.py
+++ Code Forces/Solutions/Level 800/1744/1744B Even-Odd Increment.py
@@ -1,41 +1,37 @@
-def Even_odd_increment():
+def even_odd_increment():
     t = int(input())
     for i in range(t):
-        len_arr, no_queries = map(int, input().split())
+        len_arr, no_of_queries = map(int, input().split())
         elements = list(map(int, input().split()))
-        even_array = list(filter(lambda x: x % 2 == 0, elements))
-        odd_array = list(filter(lambda x: x % 2 == 1, elements))
-        for j in range(no_queries):
+        even, odd = 0, 0
+        for e in elements:
+            if e % 2 == 0:
+                even += 1
+            else:
+                odd += 1
+        increment = 0
+        sum_array = sum(elements)
+        for j in range(no_of_queries):
             tj, xj = map(int, input().split())
             if tj == 0:
-                # for k in range(len_arr):
-                #     if elements[k] % 2 == 0:
-                #         elements[k] += xj
                 if xj % 2 == 0:
-                    for k in range(len(even_array)):
-                        even_array[k] += xj
+                    x = even * xj
+                    increment += x
                 else:
-                    k = 0
-                    while k < len(even_array):
-                        odd_array.append(even_array[k] + xj)
-                        even_array.pop(k)
+                    x = even * xj
+                    increment += x
+                    odd += even
+                    even = 0
             else:
-                # for m in range(len_arr):
-                #     if elements[m] % 2 == 1:
-                #         elements[m] += xj
                 if xj % 2 == 0:
-                    for k in range(len(odd_array)):
-                        odd_array[k] += xj
+                    x = odd * xj
+                    increment += x
                 else:
-                    k = 0
-                    while k < len(odd_array):
-                        even_array.append(odd_array[k] + xj)
-                        odd_array.pop(k)
-            print(sum(even_array) + sum(odd_array))
+                    x = odd * xj
+                    increment += x
+                    even += odd
+                    odd = 0
+            print(sum_array + increment)
 
 
-def main():
-    Even_odd_increment()
-
-
-main()
+even_odd_increment()
